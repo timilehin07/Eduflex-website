c0a44fb1e7b9061938d6ccf26719fddca66c2961
Afolayan Tim <afolayantimi555@gmail.com>
2025-10-06 03:22:11 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// Vite config for project with 'client' as source folder
 export default defineConfig({
-  root: "client", // source folder
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: "dist", // relative to root (client/dist)
+    outDir: path.resolve(__dirname, "dist"), // <- move build to root
     emptyOutDir: true,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client"),
+      "@": path.resolve(__dirname, "./client"),
     },
   },
   plugins: [react()],
 });
- 
